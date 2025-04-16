5832a1e6f7439b32444a93ef8eab4e24384dd8bc
arthurkeeng <keengarthurchima@gmail.com>
2025-04-16 16:58:41 +0100

    removing arcjet

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,11 +6,18 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
+ 
   baseDirectory: __dirname,
 });
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": "off",
+      "no-unused-vars": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
