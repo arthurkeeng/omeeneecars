563b04f94e9880f5331a5d6b12f43c2cddb72ee4
arthurkeeng <keengarthurchima@gmail.com>
2025-04-21 19:38:49 +0100

    force dynamic

--- app/(admin)/admin/page.tsx
+++ app/(admin)/admin/page.tsx
@@ -1,9 +1,11 @@
 import { getDashboardData } from "@/actions/admin";
 import { Dashboard } from "./_components/dashboard";
 
+export const dynamic = "force-dynamic";
+
 export const metadata = {
-  title: "Dashboard | Vehiql Admin",
-  description: "Admin dashboard for Vehiql car marketplace",
+  title: "Dashboard | Omeeneecars Admin",
+  description: "Admin dashboard for omeeneecar marketplace",
 };
 
 export default async function AdminDashboardPage() {
